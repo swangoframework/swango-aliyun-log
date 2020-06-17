8799fcf21379431d04e780599563258494df1e7f
子旭 苏 <fdream.lhl@foxmail.com>
2020-06-17 15:26:39 +0800

    Swango http server exception compatible

--- src/Exception/IncompleteResultsException.php
+++ src/Exception/IncompleteResultsException.php
@@ -4,4 +4,10 @@ class IncompleteResultsException extends \Swango\Aliyun\Log\Exception {
     public function __construct(string $requestId) {
         parent::__construct('IncompleteResults', 'Incomplete results. Change query or try again.', $requestId);
     }
+    public function getSwangoCnMsg(): string {
+        return '事件引擎查询结果不完整，请重试或改变筛选条件';
+    }
+    public function getSwangoCode(): int {
+        return 200;
+    }
 }
