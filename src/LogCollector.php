771fda5ab77be0799d83c60bfc0e49f057dff22b
fdream <fdream.lhl@foxmail.com>
2022-04-22 12:14:35 +0800

    add LogCollector

---
+++ src/LogCollector.php
@@ -0,0 +1,57 @@
+<?php
+namespace Swango\Aliyun\Log;
+use Swango\Aliyun\Log;
+class LogCollector {
+    private static \Swoole\Coroutine\Channel $channel;
+    public static function start(int $concurrency = 8, int $queue_size = 128) {
+        self::$channel = new \Swoole\Coroutine\Channel($queue_size);
+        \Swoole\Coroutine\parallel($concurrency, '\\SystemTool\\LogCollector::loop');
+    }
+    public static function stop() {
+        self::$channel->close();
+    }
+    public static function loop() {
+        while (($request = self::$channel->pop()) instanceof Log\Models\Request\PutLogs) {
+            self::sendLog($request);
+            unset($request);
+        }
+    }
+    public static function sendLog(Log\Models\Request\PutLogs $request): bool {
+        try {
+            $client = new Log\Action\PutLogs();
+            $client->execute($request);
+            return true;
+        } catch (\ApiErrorException\ApiTimeoutException $e) {
+            // 连接超时的请求，放回队列
+            \FileLog::logThrowable($e, \Swango\Environment::getDir()->log . 'error/', 'LogCollector');
+            if (isset(self::$channel) && ! self::$channel->push($request, 0.001)) {
+                trigger_error('Channel push error: ' . self::$channel->errCode);
+            }
+        } catch (\Throwable $e) {
+            $title = 'LogCollector';
+            if ('Required fields are missing' === $e->getMessage()) {
+                $title .= ' ' . \Json::encode($request->getLogItems()) . ' ';
+            }
+            \FileLog::logThrowable($e, \Swango\Environment::getDir()->log . 'error/', $title);
+        }
+        return false;
+    }
+    public static function buildLogItem(int $time, array &$data): Log\Models\LogItem {
+        return new Log\Models\LogItem($time, $data);
+    }
+    public static function addLog(string             $project,
+                                  string             $log_store,
+                                  string             $topic,
+                                  Log\Models\LogItem ...$log_item): bool {
+        $request = new Log\Models\Request\PutLogs($project, $log_store, $topic, logitems: $log_item);
+        if (isset(self::$channel)) {
+            $result = self::$channel->push($request, 0.001);
+            if (! $result) {
+                trigger_error('Channel push error: ' . self::$channel->errCode);
+            }
+            return $result;
+        } else {
+            return self::sendLog($request);
+        }
+    }
+}
