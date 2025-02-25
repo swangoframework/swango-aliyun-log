7be8be065860d3efb86ebaa69be806e209d98d7e
烈梦 <fdream.lhl@foxmail.com>
2025-02-25 11:44:14 +0800

    Block file log for PutLogs action

--- src/Action/PutLogs.php
+++ src/Action/PutLogs.php
@@ -62,7 +62,7 @@ class PutLogs extends \Swango\Aliyun\Log\Client {
         ] = $this->send('POST', $project, $mem, $resource, $params, $headers);
         return new \Swango\Aliyun\Log\Models\Response\PutLogs($header);
     }
-    protected function writeLogWithStream(Psr\Http\Message\StreamInterface $log_stream): void {
+    protected function writeLogWithStream(\Psr\Http\Message\StreamInterface $log_stream): void {
         // Not write log because it's large and unreadable
     }
     protected function writeLog(string &$log_string): void {
